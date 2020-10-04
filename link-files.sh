65484c2a24822f90b446a9680884beb6f942f9fa
Tim Wong <wongkk25@users.noreply.github.com>
2020-10-04 00:52:51 -0400

    added creation of required dirs

--- link-files.sh
+++ link-files.sh
@@ -4,5 +4,9 @@
 
 CONFIG_DIR=~/.config
 
+# required directories for symlinks
+mkdir -p $CONFIG_DIR/i3
+
+# set symlinks
 ln -sf $(pwd)/i3/config $CONFIG_DIR/i3/config
 ln -sf $(pwd)/.zshrc ~/.zshrc
