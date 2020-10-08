9b752dfea0fcfd1f32f714ff18b00c0c1d8b2400
Tim Wong <wongkk25@users.noreply.github.com>
2020-10-08 11:38:29 -0400

    updated file linker

--- link-files.sh
+++ link-files.sh
@@ -6,7 +6,10 @@ CONFIG_DIR=~/.config
 
 # required directories for symlinks
 mkdir -p $CONFIG_DIR/i3
+mkdir -p $CONFIG_DIR/redshift
 
 # set symlinks
 ln -sf $(pwd)/i3/config $CONFIG_DIR/i3/config
 ln -sf $(pwd)/.zshrc ~/.zshrc
+ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/redshift/redshift.conf $CONFIG_DIR/redshift/redshift.conf
